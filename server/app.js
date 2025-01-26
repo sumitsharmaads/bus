dbc462ccd3cea4404ad98ac6cd49d739e916b4d9
Sumit Sharma <sumit.sdadhich@gmail.com>
2025-01-26 23:27:39 +0530

    changed path

--- server/app.js
+++ server/app.js
@@ -63,7 +63,7 @@ app.use("/api/v1/tours", tourRoutes);
 
 app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.use((req, res, next) => {
